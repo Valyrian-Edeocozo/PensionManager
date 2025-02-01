b55ebca91ed27ebaa142b0c76ff253f87ac796a7
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-02-02 00:17:19 +0100

    commented fluent api on model creation

--- PensionManager.Infrastructure/ApplicationDbContext.cs
+++ PensionManager.Infrastructure/ApplicationDbContext.cs
@@ -19,53 +19,53 @@ public class ApplicationDbContext : IdentityDbContext<User>
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder); // Required for Identity configurations
+            // base.OnModelCreating(modelBuilder); // Required for Identity configurations
 
-            // Configure Address as an owned entity for ApplicationUser
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.OwnsOne(u => u.Address, address =>
-                {
-                    address.Property(a => a.Street).HasColumnName("Street");
-                    address.Property(a => a.City).HasColumnName("City");
-                    address.Property(a => a.State).HasColumnName("State");
-                    address.Property(a => a.PostalCode).HasColumnName("PostalCode");
-                    address.Property(a => a.Country).HasColumnName("Country");
-                });
+            // // Configure Address as an owned entity for ApplicationUser
+            // modelBuilder.Entity<User>(entity =>
+            // {
+            //     entity.OwnsOne(u => u.Address, address =>
+            //     {
+            //         address.Property(a => a.Street).HasColumnName("Street");
+            //         address.Property(a => a.City).HasColumnName("City");
+            //         address.Property(a => a.State).HasColumnName("State");
+            //         address.Property(a => a.PostalCode).HasColumnName("PostalCode");
+            //         address.Property(a => a.Country).HasColumnName("Country");
+            //     });
 
-                // // Relationships
-                // entity.HasOne(u => u.Employer)
-                //       .WithMany(e => e.User)
-                //       .HasForeignKey(u => u.EmployerId)
-                //       .OnDelete(DeleteBehavior.Restrict);
+            //     // // Relationships
+            //     // entity.HasOne(u => u.Employer)
+            //     //       .WithMany(e => e.User)
+            //     //       .HasForeignKey(u => u.EmployerId)
+            //     //       .OnDelete(DeleteBehavior.Restrict);
 
-                entity.HasOne(u => u.PensionPlan)
-                      .WithMany(p => p.Users)
-                      .HasForeignKey(u => u.PensionPlanId)
-                      .OnDelete(DeleteBehavior.Restrict);
+            //     entity.HasOne(u => u.PensionPlan)
+            //           .WithMany(p => p.Users)
+            //           .HasForeignKey(u => u.PensionPlanId)
+            //           .OnDelete(DeleteBehavior.Restrict);
 
-                // Indexes
-                entity.HasIndex(u => u.NationalId).IsUnique();
-            });
+            //     // Indexes
+            //     entity.HasIndex(u => u.NationalId).IsUnique();
+            // });
 
-            // Configure Employer
-            modelBuilder.Entity<Employer>(entity =>
-            {
-                entity.HasKey(e => e.EmployerId);
-                entity.OwnsOne(e => e.Address, address =>
-                {
-                    address.Property(a => a.Street).HasColumnName("Street");
-                    address.Property(a => a.City).HasColumnName("City");
-                    address.Property(a => a.State).HasColumnName("State");
-                    address.Property(a => a.PostalCode).HasColumnName("PostalCode");
-                    address.Property(a => a.Country).HasColumnName("Country");
-                });
-            });
+            // // Configure Employer
+            // modelBuilder.Entity<Employer>(entity =>
+            // {
+            //     entity.HasKey(e => e.EmployerId);
+            //     entity.OwnsOne(e => e.Address, address =>
+            //     {
+            //         address.Property(a => a.Street).HasColumnName("Street");
+            //         address.Property(a => a.City).HasColumnName("City");
+            //         address.Property(a => a.State).HasColumnName("State");
+            //         address.Property(a => a.PostalCode).HasColumnName("PostalCode");
+            //         address.Property(a => a.Country).HasColumnName("Country");
+            //     });
+            // });
 
-            // Configure PensionPlan
-            modelBuilder.Entity<PensionPlan>(entity =>
-            {
-                entity.HasKey(p => p.PensionPlanId);
-            });
+            // // Configure PensionPlan
+            // modelBuilder.Entity<PensionPlan>(entity =>
+            // {
+            //     entity.HasKey(p => p.PensionPlanId);
+            // });
         }
 }
